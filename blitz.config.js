155bb62421c79eac17690d2783bb2526fc3a87a6
Brandon Bayer <b@bayer.ws>
2021-03-08 18:30:31 -0500

    Update blitz.config.js

--- blitz.config.js
+++ blitz.config.js
@@ -1,7 +1,6 @@
 const { sessionMiddleware, simpleRolesIsAuthorized } = require("blitz")
 
 module.exports = {
-  log: { level: "error" },
   middleware: [
     sessionMiddleware({
       isAuthorized: simpleRolesIsAuthorized,
